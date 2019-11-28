d7d9ebc87b1d3f51089e2c2c1ea799fb43cdafbc
Codexiaoyi <874183200@.com>
2019-11-28 21:58:34 +0800

    [ADD]样式调整

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/file">File</router-link>
+        <router-link to="/">主页</router-link>
+        <router-link to="/about">关于</router-link>
+        <router-link to="/file">文件</router-link>
+        <router-link to="/login">登录</router-link>
+        <router-link to="/register">注册</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<!-- <style lang="stylus">
-    #app font-family 'Avenir',
-    Helvetica,
-    Arial,
-    sans-serif -webkit-font-smoothing antialiased -moz-osx-font-smoothing grayscale text-align center color #2c3e50 margin-top 60px
-</style> -->
+<style lang="stylus">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #aaaaaa;
+}
+
+#nav {
+  padding: 10px;
+  background: #3c4245;
+  text-align center 
+}
+
+a {
+  margin-left 20px;
+  font-weight: bold;
+  color: #ecfcff;
+
+  &.router-link-exact-active {
+    color: #42b983;
+  }
+}
+</style>
--- src/views/File.vue
+++ src/views/File.vue
@@ -1,93 +1,100 @@
 <template>
-    <div class="file">
-        <b-container>
-            <b-row>
-                <b-col cols="8">
-                    <b-form-file v-on:change="getFile($event)" browse-text="选择文件" placeholder="请选择需要上传的文件...">
-                    </b-form-file>
-                </b-col>
-                <b-col cols="4">
-                    <b-button variant="success" v-on:click="submitForm($event)">上传</b-button>
-                </b-col>
-            </b-row>
-        </b-container>
-        <div>
-            <b-table striped hover :items="items" :fields="fields">
-                    <template v-slot:cell(index)="data">
-                            {{ data.index + 1 }}
-                          </template>
-<template v-slot:cell(actions)="data">
-                    <b-button size="sm" variant="danger" @click="deleteFile(data.item.id)">删除</b-button>
-                    <b-button size="sm" variant="success" @click="downloadFile(data.item.id)">下载</b-button>
-                </template>
-<template v-slot:cell(fileName)="data">
-                    {{data.item.fileName}}
-                </template>
-<template v-slot:cell(fileSize)="data">
-                    {{data.item.fileSize}}
-                </template>
-</b-table>
-</div>
-</div>
+  <div class="file">
+    <b-container>
+      <b-row>
+        <b-col cols="8">
+          <b-form-file v-on:change="getFile($event)" browse-text="选择文件" placeholder="请选择需要上传的文件..."></b-form-file>
+        </b-col>
+        <b-col cols="4">
+          <b-button variant="success" v-on:click="submitForm($event)">上传</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div class="file_table">
+      <b-table striped hover :items="items" :fields="fields">
+        <template v-slot:cell(index)="data">{{ data.index + 1 }}</template>
+        <template v-slot:cell(actions)="data">
+          <b-button size="sm" variant="danger" @click="deleteFile(data.item.id)">删除</b-button>
+          <b-button size="sm" variant="success" @click="downloadFile(data.item.id)">下载</b-button>
+        </template>
+        <template v-slot:cell(fileName)="data">{{ data.item.fileName }}</template>
+        <template v-slot:cell(fileSize)="data">{{ data.item.fileSize }}</template>
+      </b-table>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                notice: '',
-                items: [],
-                fields: [{
-                    key: 'index',
-                    label: '序号'
-                }, {
-                    key: 'fileName',
-                    label: '文件名'
-                }, {
-                    key: 'fileSize',
-                    label: '文件大小'
-                }, {
-                    key: 'actions',
-                    label: ''
-                }],
-            }
+export default {
+  data() {
+    return {
+      notice: "",
+      items: [],
+      fields: [
+        {
+          key: "index",
+          label: "序号"
         },
-        methods: {
-            getFile(event) {
-                this.file = event.target.files[0];
-            },
-            submitForm(event) {
-                event.preventDefault();
-                let formData = new FormData();
-                formData.append("file", this.file);
-                this.$api.post('file/upload', formData, r => {
-                    this.$api.get('file/files', null, r => {
-                        this.items = r.result
-                    })
-                });
-            },
-            deleteFile(id) {
-                let sendId = id;
-                this.$api.delete('file/delete', sendId, r => {
-                    alert('删除成功');
-                    this.$api.get('file/files', null, r => {
-                        this.items = r.result
-                    })
-                })
-            },
-            downloadFile(id) {
-                // this.$api.get('file/download?id=' + id, null, r => {
-                //         console.log(r)
-                //         Window.
-                //             //window.location.href = 'file/download';
-                //     })
-                window.location.href = 'http://localhost:5000/api/file/download?id=' + id;
-            }
+        {
+          key: "fileName",
+          label: "文件名"
         },
-        beforeCreate() {
-            this.$api.get('file/files', null, r => {
-                this.items = r.result
-            });
+        {
+          key: "fileSize",
+          label: "文件大小"
         },
+        {
+          key: "actions",
+          label: ""
+        }
+      ]
+    };
+  },
+  methods: {
+    getFile(event) {
+      this.file = event.target.files[0];
+    },
+    submitForm(event) {
+      event.preventDefault();
+      let formData = new FormData();
+      formData.append("file", this.file);
+      this.$api.post("file/upload", formData, r => {
+        this.$api.get("file/files", null, r => {
+          this.items = r.result;
+        });
+      });
+    },
+    deleteFile(id) {
+      let sendId = id;
+      this.$api.delete("file/delete", sendId, r => {
+        alert("删除成功");
+        this.$api.get("file/files", null, r => {
+          this.items = r.result;
+        });
+      });
+    },
+    downloadFile(id) {
+      // this.$api.get('file/download?id=' + id, null, r => {
+      //         console.log(r)
+      //         Window.
+      //             //window.location.href = 'file/download';
+      //     })
+      window.location.href = "http://localhost:5000/api/file/download?id=" + id;
     }
+  },
+  beforeCreate() {
+    this.$api.get("file/files", null, r => {
+      this.items = r.result;
+    });
+  }
+};
 </script>
+
+<style>
+.file {
+  margin: 20px 50px;
+}
+.file_table{
+  margin-top: 20px;
+}
+</style>
